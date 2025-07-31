1c8c016050beda1bd4b42dd5e0ee913db54953b7
Emmanuel1539 <ekenedonzube1539@gmail.com>
2025-07-31 05:35:03 +0100

    Authentication resolved

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 
@@ -106,4 +108,23 @@ export async function deleteInvoice( id: string ) {
     // Revalidate paths or perform any other necessary actions after deleting the invoice
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+}
+
+export async function authenticateUser(
+    prevState: string | undefined, 
+    formData: FormData,
+    ) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid email or password.';
+                default:
+                    return  'Authentication failed.' ;
+            }
+        }
+        throw error; // Re-throw if it's not an AuthError
+    }
 }
---
+++ auth.config.ts
@@ -0,0 +1,25 @@
+import type {NextAuthConfig} from 'next-auth';
+
+export const authConfig = {
+    pages: {
+        signIn: '/login',
+    },
+    callbacks: {
+        authorized({auth, request: {nextUrl}}: any) {
+            const isLoggedIn = auth?.user;
+            // what if i want to protect other pages like /dashboard, /profile, etc.?
+            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+            const isOnProfile = nextUrl.pathname.startsWith('/profile');
+            if (isOnDashboard || isOnProfile) {
+                if (isLoggedIn) return true;
+                return false; // Redirect to login page if not logged in
+
+            } else if (isLoggedIn) {
+                return Response.redirect(new URL('/dashboard', nextUrl));
+            }
+            return true; // Allow access to other pages if not logged in
+        },
+    },
+    providers: [], // Add your authentication providers here
+} satisfies NextAuthConfig;
+
